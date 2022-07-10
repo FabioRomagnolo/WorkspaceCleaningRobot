eff5146cbcd1c2c65333acab9667911c9a86cb4f
FabioRomagnolo <romagnolo.fabio@outlook.it>
2022-07-10 10:04:44 -0700

    rviz integration with camera

---
+++ ros_1/catkin_ws/build/my_package/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/fabio/Desktop/WorkspaceCleaningRobot/ros_1/catkin_ws/src/my_package
+# Build directory: /home/fabio/Desktop/WorkspaceCleaningRobot/ros_1/catkin_ws/build/my_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ros_1/catkin_ws/devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/fabio/Desktop/WorkspaceCleaningRobot/ros_1/catkin_ws/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig-version.cmake
